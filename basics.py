4459a7f3c2fa6ef33dc00395507a5cb36f8ee8c4
GUDA ADHI YADAV <adhiyadavg@gmail.com>
2026-06-26 19:45:23 +0530

    added basic operations using print and strings

--- basics.py
+++ basics.py
@@ -1,1 +1,11 @@
+#to see any output on your screen we use print
 print("Hello, World!")
+print(2+3)
+print(4-5)
+print(3*10)
+print(10/5) #it gives floating value & it gives Quotient
+print(10//5) #it gives integer value & it gives quotient
+print(10%3) #it gives remainder
+print(9+2*3) #follow BODMAS rule
+print(8*8*8) #it gives exponential value
+print(8**3) #alternative way to do exponential calculation
