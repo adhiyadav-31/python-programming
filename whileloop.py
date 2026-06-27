0f8b7e923c793a8ebd7fbf11666792ce30c96967
GUDA ADHI YADAV <adhiyadavg@gmail.com>
2026-06-27 21:35:29 +0530

    added whileloop content

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+#while loop
+i=1
+while i<=5:
+    print(i)
+    i+=1
+#nested while loop
+i=1
+while i<=5:
+    print("happy")
+    i+=1
+    j=1
+    while(j<=5):
+        print("days")
+        j+=1
