14c85808cd985f9204081c30cc868e8d1c93bd69
GUDA ADHI YADAV <adhiyadavg@gmail.com>
2026-06-27 13:07:36 +0530

    added operators content

---
+++ operators.py
@@ -0,0 +1,40 @@
+#operators are used to perform operations on variables
+#Arithmetic,relational operators
+#Arithmetic Operators
+a=3
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+a+=2
+print(a)
+
+#a++ is not used in python
+
+#assignment operators
+x=6
+y=2
+
+x,y=7,9
+print(x)
+print(y)
+
+#relational operators <,<=.>,>=,==,!=
+u=10
+v=20
+print(u>v)
+print(u==v)
+print(u!=v)
+
+#logical operators(and,or,not)
+
+print(u>v and u<20)
+print(u>v or u<20)
+
+result=True
+print(not result)
