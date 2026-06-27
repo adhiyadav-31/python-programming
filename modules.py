071798edf72362ba6ac9d53775a111a13c656945
GUDA ADHI YADAV <adhiyadavg@gmail.com>
2026-06-27 15:35:19 +0530

    added modules content

---
+++ modules.py
@@ -0,0 +1,28 @@
+import math
+num=25
+result=math.sqrt(num)
+print(result)
+
+#if u do not want all the functions in your program then use
+from math import sqrt
+num1=16
+result1=sqrt(num1)
+
+number=59.4
+print(math.ceil(number))
+print(math.floor(number))
+print(math.pow(3,2))
+
+import math as m
+print(m.log(10))
+
+#swapping of two variables
+a=5
+b=6
+
+a=a+b
+b=a-b
+a=a-b
+
+print(a)
+print(b)
